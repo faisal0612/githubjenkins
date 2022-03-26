53f566e99fa63348f05400eb1b811e5625a71461
faisal0612 <94895189+faisal0612@users.noreply.github.com>
2022-03-26 06:58:03 -0700

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,4 +1,2 @@
-echo "This is my Jenkins second code trigger Test using webhooks" 
-Testing again with the new permissions.
-Third test without the shell command,
-Fourth test with different chmod.
+echo "This is my Jenkins second code trigger Test using webhooks" Testing again with the new permissions.Third test without the shell command,Fourth test with different chmod.
+
